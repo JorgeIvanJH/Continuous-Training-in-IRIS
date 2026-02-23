3e54e3ae07d5fd563ffa5c9aa2eb6888dd82f6b8
JorgeIvanJH <jorgeivan.jaramillo.herrera@gmail.com>
2026-02-23 11:56:11 +0000

    base

---
+++ iris_autoconf.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+
+iris session IRIS <<'EOF'
+
+/* Install IPM/ZPM client if you still need that first
+   (your original snippet did this already) */
+s version="latest" s r=##class(%Net.HttpRequest).%New(),r.Server="pm.community.intersystems.com",r.SSLConfiguration="ISC.FeatureTracker.SSL.Config" d r.Get("/packages/zpm/"_version_"/installer"),$system.OBJ.LoadStream(r.HttpResponse.Data,"c")
+
+/* Configure registry */
+zpm
+repo -r -n registry -url https://pm.community.intersystems.com/ -user "" -pass ""
+install csvgenpy
+quit
+
+/* Import and Compile the MockPackage */
+/* The "ck" flags will Compile and Keep the source */
+Do $system.OBJ.Import("/usr/irissys/mgr/MockPackage", "ck")
+
+/* Upload csv data ONCE to Table Automatically using csvgenpy */
+SET exists = ##class(%SYSTEM.SQL.Schema).TableExists("MockPackage.NoShowsAppointments")
+IF 'exists {   do ##class(shvarov.csvgenpy.csv).Generate("/dur/data/healthcare_noshows_appointments.csv","NoShowsAppointments","MockPackage")   }
+
+/* Enable Analytics */
+do EnableDeepSee^%SYS.cspServer("/csp/user/") # Enable for USER namespace
+
+
+halt
+EOF
