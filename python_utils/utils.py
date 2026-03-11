af35bc6b3d50291650bbea7c6778a01b97f690be
JorgeIvanJH <jorgeivan.jaramillo.herrera@gmail.com>
2026-03-11 15:13:03 +0000

    plot model inference on logs

--- python_utils/utils.py
+++ python_utils/utils.py
@@ -1,7 +1,12 @@
+import os
 from functools import wraps
+import matplotlib.pyplot as plt
+import numpy as np
+import mlflow
 import pandas as pd
 import lightgbm as lgb
 import time
+
 
 def measure_time_decorator(func):
     @wraps(func)
@@ -11,55 +16,47 @@ def measure_time_decorator(func):
         end_time = time.time()
         elapsed_time = end_time - start_time
         return result, elapsed_time
+
     return wrapper
 
-def noshows_data_preprocessing(df: pd.DataFrame):
 
-    df = df.drop(columns=['appointmentid', 'patientid'])
-    df = df.astype({
-                'gender': 'category',
-                'neighbourhood': 'category',
-                'scheduledday': 'datetime64[ns]',
-                'appointmentday': 'datetime64[ns]',
-                })
-    # DATA PREPARATION (slight difference for LightGBM)
-    date_cols = ["scheduledday", "appointmentday"]
+def plot_inference(self, Xtrain, Ytrain, Xtest, Ytest, oldrun, newrun):
+    """
+    Plots the inference results of the old and new models along with the training and testing data.
+    Assumes the path to the models' file is defined in the MODELSPATH parameter in an objectscript class
+    Args:
+        self: The instance of the class calling this function, used to access parameters.
+        Xtrain (pd.DataFrame): Training features.
+        Ytrain (pd.Series): Training labels.
+        Xtest (pd.DataFrame): Testing features.
+        Ytest (pd.Series): Testing labels.
+        oldrun (mlflow.entities.Run): The MLflow run object for the old model.
+        newrun (mlflow.entities.Run): The MLflow run object for the new model.
+    """
+    oldrunname = oldrun.get("tags.mlflow.runName")
+    newrunname = newrun.get("tags.mlflow.runName")
 
-    # 1. Extract useful components
-    for col in date_cols:
-        df[col + "_year"] = df[col].dt.year
-        df[col + "_month"] = df[col].dt.month
-        df[col + "_day"] = df[col].dt.day
-        df[col + "_dow"] = df[col].dt.dayofweek         # 0=Mon, 6=Sun
-        df[col + "_hour"] = df[col].dt.hour
-        df[col + "_is_weekend"] = (df[col].dt.dayofweek >= 5).astype("int8")
-        
-        # Optional: Part-of-day feature
-        df[col + "_part_of_day"] = pd.cut(
-            df[col].dt.hour,
-            bins=[-1, 6, 12, 17, 24],
-            labels=[0, 1, 2, 3],        # 0=night,1=morning,2=afternoon,3=evening
-            ordered=True
-        ).astype("int8")
-    df = df.drop(columns=date_cols)
+    oldmodel = mlflow.sklearn.load_model(
+        os.path.join(eval("""self._GetParameter("MODELSPATH")"""), oldrun.run_id)
+    )
+    newmodel = mlflow.sklearn.load_model(
+        os.path.join(eval("""self._GetParameter("MODELSPATH")"""), newrun.run_id)
+    )
 
-    # DROP CATEGORICAL FEATURES (just for now)
-    df = df.drop(columns=["gender", "neighbourhood"])
-    y = df["showed_up"]
-    X = df.drop(columns=["showed_up"])
-    return X, y
-
-def load_lightgbm_model(model_path: str):
-    bst = lgb.Booster(model_file=model_path)
-    return bst
-
-def model_inference(model, X: pd.DataFrame):
-    y_pred = model.predict(X)
-    return y_pred
-
-@measure_time_decorator
-def inference_pipeline(model_path: str, df: pd.DataFrame):
-    model = load_lightgbm_model(model_path)
-    X, _ = noshows_data_preprocessing(df)
-    y_pred = model_inference(model, X)
-    return y_pred
+    line_x = np.linspace(Xtest.min(), Xtest.max(), 100).reshape(-1, 1)
+    line_y_old = oldmodel.predict(line_x)
+    line_y_new = newmodel.predict(line_x)
+    plt.figure(figsize=(10, 6))
+    if not Xtrain.empty and not Ytrain.empty:
+        plt.scatter(Xtrain, Ytrain, color="orange", label="Train Data")
+    if not Xtest.empty and not Ytest.empty:
+        plt.scatter(Xtest, Ytest, color="blue", label="Test Data")
+    plt.plot(line_x, line_y_old, color="red", label=f"Old Model: {oldrunname}")
+    plt.plot(line_x, line_y_new, color="green", label=f"New Model: {newrunname}")
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.title("Model Comparison")
+    plt.legend()
+    plt.grid()
+    plt.savefig(f"/dur/log/model_comparison_{oldrunname}_vs_{newrunname}.png")
+    plt.close()
